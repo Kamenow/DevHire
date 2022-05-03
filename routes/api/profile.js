28ebd52483ec51101313662d8ad56d7ef0ff0f63
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-03 16:21:41 +0300

    added get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,4 +112,17 @@ router.post(
   }
 );
 
+// @route  GET api/profile
+// @desc   Get all Profiles
+// @access Private
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
