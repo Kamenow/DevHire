05e37774ed2cc55f7a4650ba66fd52a2479b43a1
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-03 13:35:50 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Yo');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
