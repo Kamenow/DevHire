ecccafc8c66e050b402d9b8c13e832fc7d5882c8
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-03 14:55:23 +0300

    added auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth.js');
+
+const User = require('../../models/User.js');
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get('/', (req, res) => {
-  res.send('auth works');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error');
+  }
 });
 
 module.exports = router;
