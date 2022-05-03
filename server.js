7ede960f1fd3694fa678ac9a8e16d9f82f1392b6
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-03 13:51:21 +0300

    added routes

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const connectDB = require('./config/db.js');
 
 const app = express();
 
+// Connect DB
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+// Define routes
+app.use('/api/users', require('./routes/api/users.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
 
 app.get('/', (req, res) => {
   res.send('Yo');
