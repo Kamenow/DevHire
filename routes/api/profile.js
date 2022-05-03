cc3e8b7f5142c8b25194c873f8dc6dc3d880f572
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-03 16:21:50 +0300

    added get profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,4 +125,27 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access Private
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
