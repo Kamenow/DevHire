c6cbd755a23df3e25283db884e5d914f3c7dc8f4
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-04 14:13:30 +0300

    added register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,20 @@
-import logo from './logo.svg';
 import React, { Fragment } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar/Navbar.js';
 import Landing from './components/layout/Landing/Landing.js';
 import Login from './components/layout/auth/Login/Login.js';
+import Register from './components/layout/auth/Register/Register.js';
 import './App.css';
 
-function App() {
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
     <BrowserRouter>
       <Fragment>
         <Navbar />
         <Routes>
           <Route path='/' element={<Landing />}></Route>
           <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
         </Routes>
       </Fragment>
     </BrowserRouter>
---
+++ client/src/components/layout/auth/Register/Register.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+import './Register.css';
+
+const Register = () => {
+  const onFinish = (values) => {
+    if (values.password !== values.repassword) {
+      console.log('Failed:', values);
+      return;
+    }
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <section className='register'>
+      <Form
+        name='basic'
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
+      >
+        <Form.Item
+          label='Email'
+          name='email'
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Username'
+          name='username'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Password'
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label='Repeat password'
+          name='repassword'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </section>
+  );
+};
+
+export default Register;
