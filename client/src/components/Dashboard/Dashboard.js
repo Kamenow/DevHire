7edabbb4df6859294a2701fe50120028419e0bd4
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-05 02:15:42 +0300

    added profile reducer

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Dashboard = () => {
+import { getCurrentProfile } from '../../actions/profile.js';
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
-export default Dashboard;
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types.js';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repost: [],
+  loading: true,
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
