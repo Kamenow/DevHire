1059126cfd4fd3dc964a4cc3a85dd84744a3b08c
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-08 16:17:24 +0300

    added update profile type

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types.js';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from '../actions/types.js';
 
 const initialState = {
   profile: null,
@@ -14,6 +20,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
