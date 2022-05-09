95abef97c4386ab47728938a67b47c66c8433c21
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-10 01:02:47 +0300

    added delete post reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,5 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types.js';
+import {
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+} from '../actions/types.js';
 
 const initialState = {
   posts: [],
@@ -31,6 +36,11 @@ export default function (state = initialState, action) {
           post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
         loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
       };
     default:
       return state;
