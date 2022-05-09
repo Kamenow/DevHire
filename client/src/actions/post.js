d6a6ddc1afb0ecea8fd8bc31e19456ed47f0ecf1
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-10 00:50:32 +0300

    added like & unlike action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert.js';
-import { GET_POSTS, POST_ERROR } from './types.js';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types.js';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -10,6 +10,46 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText || 'Something went wrong',
+        status: err.response.status || 500,
+      },
+    });
+  }
+};
+
+// Add like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText || 'Something went wrong',
+        status: err.response.status || 500,
+      },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
     });
   } catch (err) {
     dispatch({
