d3603cc69bb53037af82a1d9931af44ffa96ab53
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-09 14:46:02 +0300

    added profiles component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ import Login from './components/layout/auth/Login/Login.js';
 import Register from './components/layout/auth/Register/Register.js';
 import AlertDisplay from './components/layout/Alert/AlertDisplay.js';
 import Dashboard from './components/Dashboard/Dashboard.js';
+import CreateProfile from './components/Profile-Form/CreateProfile.js';
+import EditProfile from './components/Profile-Form/EditProfile.js';
+import AddExperience from './components/Profile-Form/AddExperience.js';
+import AddEducation from './components/Profile-Form/AddEducation.js';
+import Profiles from './components/Profiles/Profiles.js';
 import PrivateRoute from './components/routing/PrivateRoute.js';
 // Redux
 import { Provider } from 'react-redux';
@@ -14,10 +19,6 @@ import { loadUser } from './actions/auth.js';
 import setAuthToken from './utils/setAuthToken.js';
 // Style
 import './App.css';
-import CreateProfile from './components/Profile-Form/CreateProfile.js';
-import EditProfile from './components/Profile-Form/EditProfile.js';
-import AddExperience from './components/Profile-Form/AddExperience.js';
-import AddEducation from './components/Profile-Form/AddEducation.js';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -38,6 +39,7 @@ const App = () => {
             <Route path='/' element={<Landing />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/profiles' element={<Profiles />} />
             <Route
               path='/create-profile'
               element={<PrivateRoute component={CreateProfile} />}
---
+++ client/src/components/Profiles/Profiles.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Spin } from 'antd';
+
+import { getProfiles } from '../../actions/profile.js';
+import ProfileItem from './ProfileItem.js';
+
+import './Profiles.css';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Spin />
+      ) : (
+        <Fragment>
+          <div className='title'>
+            <h1>Developers</h1>
+          </div>
+          <section className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <p>No profiles</p>
+            )}
+          </section>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
