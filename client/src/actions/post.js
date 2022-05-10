5649bff79cdb5267e709e2957b15e119177ff433
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-10 16:48:07 +0300

    get single post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert.js';
 import {
   ADD_POST,
   DELETE_POST,
+  GET_POST,
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
@@ -107,6 +108,26 @@ export const addPost = (formData) => async (dispatch) => {
     });
 
     dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText || 'Something went wrong',
+        status: err.response.status || 500,
+      },
+    });
+  }
+};
+
+// Get post
+export const getPost = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${postId}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
