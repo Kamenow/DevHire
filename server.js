a6cb218076431d806b837ddb1d39268b1155dbc0
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-05-10 22:42:17 +0300

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
+const path = require('path');
 
 const app = express();
 
@@ -9,17 +10,23 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
 // Define routes
 app.use('/api/users', require('./routes/api/users.js'));
 app.use('/api/auth', require('./routes/api/auth.js'));
 app.use('/api/profile', require('./routes/api/profile.js'));
 app.use('/api/posts', require('./routes/api/posts.js'));
 
-app.get('/', (req, res) => {
-  res.send('Yo');
-});
+// Serve static assets in production
+if (proccess.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
